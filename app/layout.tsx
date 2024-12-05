36f30a8f9948e918881c2fe4d9b65adeb6113a82
minibarcoders <minibar_coders0c@icloud.com>
2024-12-05 08:26:49 +0100

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/ui/Header'
-import GoogleAnalytics from './components/GoogleAnalytics'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Fixed or Custom - Tech Reviews & Insights',
-  description: 'Your trusted source for tech reviews and insights',
+  title: 'Fixed or Custom',
+  description: 'Tech reviews and guides to help you make informed decisions',
 }
 
 export default function RootLayout({
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <GoogleAnalytics />
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   )
